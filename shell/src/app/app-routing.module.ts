d39e09daee1b2119406286d7e24afb3081d0ca92
Nofar Edan <noedan@microsoft.com>
2022-01-06 20:44:35 +0200

    First commit

---
+++ shell/src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation-runtime';
+import '../main.d';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'remote',
+    loadChildren: () => import('mfe1/Module').then(m => {
+      return m.MicrofrontendModule;
+    })
+    // loadChildren: () =>
+    //   loadRemoteModule({
+    //     remoteEntry: 'http://localhost:3000/remoteEntry.js',
+    //     remoteName: 'mfe1',
+    //     exposedModule: './Module',
+    //   }).then((m) => {
+    //     return m.MicrofrontendModule;
+    //   }),
+  },
+  {
+    path: 'remote1',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:7000/remoteEntry.js',
+        remoteName: 'mfe2',
+        exposedModule: './Module1',
+      }).then((m) => {
+        return m.Microfrontend1Module;
+      }),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
